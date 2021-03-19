bf05662e19f7c026a4928902ecf3b19b0bfa9afc
Ian Titor <ianpatricktan@gmail.com>
2021-03-19 11:35:33 -0400

    fixed bug with leaving gc

--- discordselfbot/__init__.py
+++ discordselfbot/__init__.py
@@ -64,6 +64,6 @@ class bot:
         self.driver.find_element_by_xpath('//*[@class="item-1tOPte labelContainer-1BLJti colorDanger-2qLCe1 colorDefault-2K3EoJ"]').click()
     
     def leaveDm(self):
-        href = self.driver.current_url.replace('https://discord.com/', '')
+        href = self.driver.current_url.replace('https://discord.com', '')
         ActionChains(self.driver).context_click(self.driver.find_element_by_xpath(f'//*[@href="{href}"]')).perform()
         self.driver.find_element_by_class_name('item-1tOPte labelContainer-1BLJti colorDanger-2qLCe1 colorDefault-2K3EoJ').click()
